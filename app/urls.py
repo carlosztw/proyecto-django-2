a10e43171279ceb4de61ee32685984d7186ab485
Carlos Silva <carlossilvacerda@gmail.com>
2022-04-26 18:57:44 -0400

    creación de templates y otros

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', inicio, name='inicio'),
+    path('productos/', productos, name='productos'),
+    path('servicios/', servicios, name='servicios'),
+    path('adm-clientes/', adm_clientes, name='adm_clientes'),
+    path('adm-productos/', adm_productos, name='adm_productos'),
+    path('adm-servicios/', adm_servicios, name='adm_servicios'),
+    path('adm-trabajadores/', adm_trabajadores, name='adm_trabajadores'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def base(request):
 
-    return render(request, 'app/index.html')
+    return render(request, 'app/base.html')
+
+def inicio(request):
+
+    return render(request, 'app/inicio.html')    
+
+def productos(request):
+
+    return render(request, 'app/productos.html')  
+
+def servicios(request):
+
+    return render(request, 'app/servicios.html')
+
+def adm_clientes(request):
+
+    return render(request, 'administradores/adm_clientes.html') 
+
+def adm_productos(request):
+
+    return render(request, 'administradores/adm_productos.html')   
+
+def adm_servicios(request):
+
+    return render(request, 'administradores/adm_servicios.html')   
+
+def adm_trabajadores(request):
+
+    return render(request, 'administradores/adm_trabajadores.html')   
