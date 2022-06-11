6ba672707ef32a03480a2e7b4c48f33044632de5
DimeloDany <dan.ariasp@duocuc.cl>
2022-06-11 11:27:13 -0400

    servicio

--- otros/scripts/Script_poblado_tablas.sql
+++ otros/scripts/Script_poblado_tablas.sql
@@ -69,7 +69,7 @@ CREATE SEQUENCE seq_resn START WITH 1;
 
 DROP SEQUENCE seq_servicio;
 CREATE SEQUENCE seq_servicio START WITH 10 INCREMENT BY 10;
-INSERT INTO servicio VALUES(SEQ_SERVICIO.nextval,'Ernesto Arredondo','ern.arredondo@gmail.com','mi computador normalmente envia un fondo azul en donde se cae el sistema','01/06/2022','https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Bsodwindows10.png/640px-Bsodwindows10.png',1);
+INSERT INTO servicio VALUES(SEQ_SERVICIO.nextval,'Ernesto Arredondo','ern.arredondo@gmail.com','mi computador normalmente envia un fondo azul en donde se cae el sistema','01/06/2022','https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Bsodwindows10.png/640px-Bsodwindows10.png',5);
 
 
 ---------------------------------------------------------------
