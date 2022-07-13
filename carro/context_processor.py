e58b4a5147ef3c4e9b49822a347dff2576e0aac1
Carlos Silva <carlossilvacerda@gmail.com>
2022-07-13 11:02:27 -0400

    arreglos en el carrito

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -4,6 +4,7 @@ from multiprocessing.sharedctypes import Value
 
 def importe_total_carro(request):
     total=0
-    for key, value in request.session["carro"].items():
-        total=total+(int(value["precio"]))
+    if 'carro' in request.session:
+        for key, value in request.session["carro"].items():
+            total=total+(int(value["precio"]))
     return {"importe_total_carro":total}
